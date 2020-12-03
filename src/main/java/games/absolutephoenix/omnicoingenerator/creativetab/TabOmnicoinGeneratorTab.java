0b89377c416ceffd08168f6e40c9bdd8c92b8074
AbsolutePhoenix <44858643+AbsolutePhoenix@users.noreply.github.com>
2020-12-03 13:14:07 -0500

    Initial Commit

---
+++ src/main/java/games/absolutephoenix/omnicoingenerator/creativetab/TabOmnicoinGeneratorTab.java
@@ -0,0 +1,35 @@
+
+package games.absolutephoenix.omnicoingenerator.creativetab;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.creativetab.CreativeTabs;
+
+import games.absolutephoenix.omnicoingenerator.block.BlockOmnicoinGenerator;
+import games.absolutephoenix.omnicoingenerator.ElementsOmnicoinGenerator;
+
+@ElementsOmnicoinGenerator.ModElement.Tag
+public class TabOmnicoinGeneratorTab extends ElementsOmnicoinGenerator.ModElement {
+	public TabOmnicoinGeneratorTab(ElementsOmnicoinGenerator instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new CreativeTabs("tabomnicoingeneratortab") {
+			@SideOnly(Side.CLIENT)
+			@Override
+			public ItemStack getTabIconItem() {
+				return new ItemStack(BlockOmnicoinGenerator.block, (int) (1));
+			}
+
+			@SideOnly(Side.CLIENT)
+			public boolean hasSearchBar() {
+				return true;
+			}
+		}.setBackgroundImageName("item_search.png");
+	}
+	public static CreativeTabs tab;
+}
---
+++ src/main/java/games/absolutephoenix/omnicoingenerator/procedure/ProcedureOmnicoinGeneratorUpdateTick.java
@@ -0,0 +1,70 @@
+package games.absolutephoenix.omnicoingenerator.procedure;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntityLockableLoot;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.item.ItemStack;
+
+import games.absolutephoenix.omnicoingenerator.item.ItemOmnicoinFragment;
+import games.absolutephoenix.omnicoingenerator.ElementsOmnicoinGenerator;
+
+@ElementsOmnicoinGenerator.ModElement.Tag
+public class ProcedureOmnicoinGeneratorUpdateTick extends ElementsOmnicoinGenerator.ModElement {
+	public ProcedureOmnicoinGeneratorUpdateTick(ElementsOmnicoinGenerator instance) {
+		super(instance, 4);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure OmnicoinGeneratorUpdateTick!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure OmnicoinGeneratorUpdateTick!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure OmnicoinGeneratorUpdateTick!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure OmnicoinGeneratorUpdateTick!");
+			return;
+		}
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		if (((new Object() {
+			public int getAmount(BlockPos pos, int sltid) {
+				TileEntity inv = world.getTileEntity(pos);
+				if (inv instanceof TileEntityLockableLoot) {
+					ItemStack stack = ((TileEntityLockableLoot) inv).getStackInSlot(sltid);
+					if (stack != null)
+						return stack.getCount();
+				}
+				return 0;
+			}
+		}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (0))) < 64)) {
+			{
+				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (inv != null && (inv instanceof TileEntityLockableLoot)) {
+					ItemStack _setstack = new ItemStack(ItemOmnicoinFragment.block, (int) (1));
+					_setstack.setCount(((new Object() {
+						public int getAmount(BlockPos pos, int sltid) {
+							TileEntity inv = world.getTileEntity(pos);
+							if (inv instanceof TileEntityLockableLoot) {
+								ItemStack stack = ((TileEntityLockableLoot) inv).getStackInSlot(sltid);
+								if (stack != null)
+									return stack.getCount();
+							}
+							return 0;
+						}
+					}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (0))) + 1));
+					((TileEntityLockableLoot) inv).setInventorySlotContents((int) (0), _setstack);
+				}
+			}
+		}
+	}
+}
